e11a6d75d311b2cc0d29a5767a2918470771d903
Paul Farcasiu <paul.farcasiu@yahoo.com>
2021-01-06 20:03:58 +0200

    comment

--- app/src/main/java/com/example/proiectpiu/Curs.kt
+++ app/src/main/java/com/example/proiectpiu/Curs.kt
@@ -3,5 +3,5 @@ package com.example.proiectpiu
 import java.io.Serializable
 
 class Curs(var numeCurs: String, var profCurs: String, var prezente:Int = 0, var linkCurs: String) :Serializable {
-
+//ssssseeee
 }
