ee8b55da9346a60d93134314fec674ef00ad9d14
Rares Fechete <fechete.rares@gmail.com>
2021-01-14 02:56:38 +0200

    spinner for rankings

--- app/src/main/java/com/example/proiectpiu/RankingActivity.kt
+++ app/src/main/java/com/example/proiectpiu/RankingActivity.kt
@@ -1,20 +1,57 @@
 package com.example.proiectpiu
 
 import android.os.Bundle
-import android.widget.ListView
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.example.proiectpiu.adapters.RankAdapter
 
-class RankingActivity : AppCompatActivity() {
+class RankingActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     var rankAdapter : RankAdapter? = null
+    var spinner : Spinner? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ranking)
+
+        val spinner: Spinner = findViewById(R.id.curs_spinner)
+        spinner.onItemSelectedListener = this
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.cursuri_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+
         val listReference = findViewById<ListView>(R.id.rank_list)
-        rankAdapter = RankAdapter(this, RankingList().getRankingsCurs1())
+        rankAdapter = RankAdapter(this, RankingList().getRankingsCurs0())
         listReference.adapter = rankAdapter
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        var list: ArrayList<RankedStudent>
+
+        when (position){
+            0 -> list = RankingList().getRankingsCurs0()
+            1 ->  list =RankingList().getRankingsCurs1()
+            2 ->  list =RankingList().getRankingsCurs2()
+            3 ->  list =RankingList().getRankingsCurs3()
+            else -> {list = RankingList().getRankingsCurs0()}
+        }
+
+        val listReference = findViewById<ListView>(R.id.rank_list)
+        rankAdapter = RankAdapter(this, list)
+        listReference.adapter = rankAdapter
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 
 
--- app/src/main/java/com/example/proiectpiu/RankingList.kt
+++ app/src/main/java/com/example/proiectpiu/RankingList.kt
@@ -2,13 +2,58 @@ package com.example.proiectpiu
 
 class RankingList {
 
-    fun getRankingsCurs1() : ArrayList<RankedStudent>{
+    fun getRankingsCurs0() : ArrayList<RankedStudent>{
         val list = ArrayList<RankedStudent>()
 
         list.add(RankedStudent("Farcasiu Paul", 4.2))
         list.add(RankedStudent("Fechete Rares", 2.5))
         list.add(RankedStudent("Pereanu Alexandru", 4.5))
         list.add(RankedStudent("Vasiu Cristian", 4.3))
+        list.add(RankedStudent("Gigel Mare", 1.7))
+        list.add(RankedStudent("Ionut Vesel", 2.9))
+        list.add(RankedStudent("Maria Ana", 3.7))
+
+        return list
+    }
+
+    fun getRankingsCurs1() : ArrayList<RankedStudent>{
+        val list = ArrayList<RankedStudent>()
+
+        list.add(RankedStudent("Farcasiu Paul", 2.2))
+        list.add(RankedStudent("Fechete Rares", 3.5))
+        list.add(RankedStudent("Pereanu Alexandru", 2.5))
+        list.add(RankedStudent("Vasiu Cristian", 3.3))
+        list.add(RankedStudent("Gigel Mare", 4.7))
+        list.add(RankedStudent("Ionut Vesel", 3.9))
+        list.add(RankedStudent("Maria Ana", 1.7))
+
+        return list
+    }
+
+    fun getRankingsCurs2() : ArrayList<RankedStudent>{
+        val list = ArrayList<RankedStudent>()
+
+        list.add(RankedStudent("Farcasiu Paul", 2.9))
+        list.add(RankedStudent("Fechete Rares", 1.5))
+        list.add(RankedStudent("Pereanu Alexandru", 2.1))
+        list.add(RankedStudent("Vasiu Cristian", 3.1))
+        list.add(RankedStudent("Gigel Mare", 2.7))
+        list.add(RankedStudent("Ionut Vesel", 3.9))
+        list.add(RankedStudent("Maria Ana", 2.7))
+
+        return list
+    }
+
+    fun getRankingsCurs3() : ArrayList<RankedStudent>{
+        val list = ArrayList<RankedStudent>()
+
+        list.add(RankedStudent("Farcasiu Paul", 4.2))
+        list.add(RankedStudent("Fechete Rares", 4.5))
+        list.add(RankedStudent("Pereanu Alexandru", 4.5))
+        list.add(RankedStudent("Vasiu Cristian", 4.3))
+        list.add(RankedStudent("Gigel Mare", 4.7))
+        list.add(RankedStudent("Ionut Vesel", 3.9))
+        list.add(RankedStudent("Maria Ana", 2.7))
 
         return list
     }
